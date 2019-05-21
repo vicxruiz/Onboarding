2c9be0be6f707eae51e41677f29a21857e0a1dee
victor <ruizdvictor@gmail.com>
2019-05-21 09:03:42 -0400

    added data source logic

--- Onboarding/Controllers/OnboardingPageViewController.swift
+++ Onboarding/Controllers/OnboardingPageViewController.swift
@@ -12,17 +12,50 @@ class OnboardingPageViewController: UIPageViewController, UIPageViewControllerDa
     
     
     var pageHeadings = ["Connect With People Around The World", "Let's Build New Connections With New People", "Feel The Happiness"]
+    var pageSubheadings = ["onboarding1", "onboarding2", "onboarind3"]
+    var pageImages = ["Users will be able to go live, chat and meet with people near by.", "Connect helps you locate the people around you who are closest from your home town!", "Users will be able to go live, chat and meet with people near by."]
+    
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataSource = self
     }
     
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        
+        var index = (viewController as! OnboardingContentViewController).index
+        index -= 1
+        
+        return contentViewController(at: index)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        var index = (viewController as! OnboardingContentViewController).index
+        index += 1
+        
+        return contentViewController(at: index)
     }
+    
+    func contentViewController(at index: Int) -> OnboardingContentViewController? {
+        if index < 0 || index >= pageHeadings.count {
+            return nil
+        }
+        
+        // Create a new view controller and pass suitable data
+        let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
+        if let pageContentViewController = storyboard.instantiateViewController(withIdentifier: "OnboardingContentViewController") as? OnboardingContentViewController {
+            pageContentViewController.imageFile = pageImages[index]
+            pageContentViewController.heading = pageHeadings[index]
+            pageContentViewController.subHeading = pageSubheadings[index]
+            pageContentViewController.index = index
+            
+            return pageContentViewController
+        }
+        
+        return nil
+    }
+    
+    
 }
